c8a0a0fc89d634ac94fa788395a1957e135ccf0b
Yash <Yashawasthi854@bbdnitm.ac.in>
2026-02-01 22:39:35 +0530

    signals correction in apps.py

--- megapixel/accounts/apps.py
+++ megapixel/accounts/apps.py
@@ -5,7 +5,5 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     def ready(self):
-        if os.getenv("DJANGO_SUPERUSER_USERNAME") and os.getenv("DJANGO_SUPERUSER_EMAIL") and os.getenv("DJANGO_SUPERUSER_PASSWORD"):
-            from .signals import create_user_profile    
-            create_user_profile()
+        import accounts.signals
             
