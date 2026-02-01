4bcf43f16cc57c7338b6913b1d1ea4e1c38aa8fa
Yash <Yashawasthi854@bbdnitm.ac.in>
2026-02-01 22:24:27 +0530

    changes in accounts/apps.py

--- megapixel/accounts/apps.py
+++ megapixel/accounts/apps.py
@@ -1,7 +1,25 @@
 from django.apps import AppConfig
+import os
 
 class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        import accounts.signals
+        from django.contrib.auth.models import User
+
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and password:
+            user, created = User.objects.get_or_create(
+                username=username,
+                defaults={"email": email}
+            )
+
+            # 🔥 FORCE permissions (THIS IS THE FIX)
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(password)
+            user.save()
