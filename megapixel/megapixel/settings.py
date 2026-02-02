0ea9fdfba5bbb899e87e3a54ee3d825962cff210
Yash <Yashawasthi854@bbdnitm.ac.in>
2026-02-02 10:41:29 +0530

    initializing supabase

--- megapixel/megapixel/settings.py
+++ megapixel/megapixel/settings.py
@@ -90,11 +90,13 @@ WSGI_APPLICATION = 'megapixel.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/profile/'
